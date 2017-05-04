e12f4e158d87851cffc6d3a04a4f26aef053d9f7
Surendra Nath <Surendra.nath@phm.co.uk>
2017-05-04 17:09:42 +0100

    Update to the program to run the azure services

--- src/client/Phm.MobileSp.Cms/Program.cs
+++ src/client/Phm.MobileSp.Cms/Program.cs
@@ -1,15 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Phm Group Ltd.">
+//   2017
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
-namespace MobileSP_CMS
+namespace Phm.MobileSp.Cms
 {
+    using System;
+    using System.IO;
+
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
         public static void Main(string[] args)
         {
             var config = new ConfigurationBuilder()
@@ -17,13 +33,24 @@ namespace MobileSP_CMS
                 .AddEnvironmentVariables(prefix: "ASPNETCORE_")
                 .Build();
 
-            var host = new WebHostBuilder()
+            var hostBuilder = new WebHostBuilder()
                 .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Phm.MobileSp.Cms.Startup>()
-                .Build();
+                .UseApplicationInsights();
+
+            var regionName = Environment.GetEnvironmentVariable("REGION_NAME");
+            if (regionName != null)
+            {
+                hostBuilder.UseAzureAppServices();
+            }
+
+            var host = hostBuilder.Build();
+
+            host.Run();
+
 
             host.Run();
         }
