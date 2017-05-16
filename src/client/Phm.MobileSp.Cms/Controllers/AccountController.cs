bb0892b1d150f0659aa7dd266d166fb6c0c6c36a
Surendra Nath <Surendra.nath@phm.co.uk>
2017-05-16 15:46:31 +0100

    More tracking to solve error on live

--- src/client/Phm.MobileSp.Cms/Controllers/AccountController.cs
+++ src/client/Phm.MobileSp.Cms/Controllers/AccountController.cs
@@ -1,30 +1,43 @@
-﻿using System.Collections.Generic;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
-using Phm.MobileSp.Cms.Core.Models;
-using Phm.MobileSp.Cms.Core.Models.Interfaces;
-using Phm.MobileSp.Cms.Helpers.Attributes;
-using Phm.MobileSp.Cms.Infrastructure;
-using Phm.MobileSp.Cms.Infrastructure.Repositories.Interfaces;
-
-namespace Phm.MobileSp.Cms.Controllers
+﻿namespace Phm.MobileSp.Cms.Controllers
 {
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
 
     using Microsoft.ApplicationInsights;
-    using Microsoft.AspNetCore.Http.Authentication;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Caching.Memory;
 
     using Newtonsoft.Json;
+
+    using Phm.MobileSp.Cms.Core.Models;
+    using Phm.MobileSp.Cms.Core.Models.Interfaces;
+    using Phm.MobileSp.Cms.Helpers.Attributes;
+    using Phm.MobileSp.Cms.Infrastructure;
+    using Phm.MobileSp.Cms.Infrastructure.Repositories.Interfaces;
 
     [AiHandleError]
     public class AccountController : CacheController
     {
-        private readonly IUserRepository _userRepository;
+        /// <summary>
+        /// The user repository.
+        /// </summary>
+        private readonly IUserRepository userRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AccountController"/> class.
+        /// </summary>
+        /// <param name="memoryCache">
+        /// The memory cache.
+        /// </param>
+        /// <param name="userRepository">
+        /// The user repository.
+        /// </param>
         public AccountController(IMemoryCache memoryCache, IUserRepository userRepository) : base(memoryCache)
         {
-            _userRepository = userRepository;
+            this.userRepository = userRepository;
         }
 
         [AiHandleError]
@@ -52,56 +65,55 @@ namespace Phm.MobileSp.Cms.Controllers
         {
             var tracking = new TelemetryClient();
             ClaimsPrincipal claimsPrinciple;
+            this.ViewData["ReturnUrl"] = returnUrl;
+            this.userRepository.SetAuthToken(Constants.CstAccesstoken);
+
+            var user = await userRepository.GetUserAsync(loginDetails);
+
+            if (!user.ValidUser)
+            {
+                return View(loginDetails);
+            }
+
+            SetupRepositories(user);
+            var claims = new List<Claim>
+                             {
+                                 new Claim("sessionguid", user.SessionGuid),
+                                 new Claim("userid", user.UserDetails.Id.ToString()),
+                                 new Claim("currentmarketid", user.UserDetails.DefaultMarketId.ToString()),
+                                 new Claim("name", user.UserDetails.FirstName)
+                             };
+           tracking.TrackTrace("Claims Issue", new Dictionary<string, string> { { "claims", JsonConvert.SerializeObject(claims)} });
+            
+
             try
             {
-                ViewData["ReturnUrl"] = returnUrl;
-                _userRepository.SetAuthToken(Constants.CstAccesstoken);
-
-                var user = await _userRepository.GetUserAsync(loginDetails);
-
-                if (!user.ValidUser)
-                {
-                    return View(loginDetails);
-                }
-
-                SetupRepositories(user);
-                var claims = new List<Claim>
-                                 {
-                                     new Claim("sessionguid", user.SessionGuid),
-                                     new Claim("userid", user.UserDetails.Id.ToString()),
-                                     new Claim("currentmarketid", user.UserDetails.DefaultMarketId.ToString()),
-                                     new Claim("name", user.UserDetails.FirstName)
-                                 };
-                tracking.TrackEvent(
-                    "AddClaims",
-                    new Dictionary<string, string>() { { "claims", JsonConvert.SerializeObject(claims) } });
-                try
-                {
-                    var id = new ClaimsIdentity(claims, "password", "MobileSP", user.UserRoles.FirstOrDefault());
-                    claimsPrinciple = new ClaimsPrincipal(id);
-                }
-                catch (System.Exception ex)
-                {
-                    tracking.TrackException(ex);
-                    throw;
-                }
-
-                await HttpContext.Authentication.SignInAsync("MobileSPAuthCookie", claimsPrinciple);
-
-                return LocalRedirect(returnUrl);
+                var id = new ClaimsIdentity(claims, "password");
+                tracking.TrackTrace("ClaimsId", SeverityLevel.Information, new Dictionary<string, string> { { "ClaimsId", JsonConvert.SerializeObject(id) } });
+                claimsPrinciple = new ClaimsPrincipal(id);
+                tracking.TrackTrace("ClaimsPrinciple", SeverityLevel.Information, new Dictionary<string, string> { { "ClaimsPrinciple", JsonConvert.SerializeObject(claimsPrinciple) } });
             }
             catch (System.Exception ex)
             {
-
-
-                throw;
+                tracking.TrackException(ex);
+                return View(loginDetails);
             }
+
+            await this.HttpContext.Authentication.SignInAsync("MobileSPAuthCookie", claimsPrinciple);
+
+            return this.LocalRedirect(returnUrl);
         }
 
+        /// <summary>
+        /// The setup repositories.
+        /// </summary>
+        /// <param name="applicationUser">
+        /// The application user.
+        /// </param>
         private void SetupRepositories(IApplicationUser applicationUser)
         {
-            _userRepository.SetAuthToken(applicationUser.SessionGuid);
-            _userRepository.SetMarketId(applicationUser.UserDetails.DefaultMarketId);
+            this.userRepository.SetAuthToken(applicationUser.SessionGuid);
+            this.userRepository.SetMarketId(applicationUser.UserDetails.DefaultMarketId);
         }
 
         [AiHandleError]
