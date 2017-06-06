03ab88fa8e30b7693287e8359a4c0894b37b12d5
Surendra Nath <Surendra.nath@phm.co.uk>
2017-06-06 17:18:10 +0100

    Re-arrange folder structure in ps file.

--- projectfiles/webPack.ps1
+++ projectfiles/webPack.ps1
@@ -1,3 +1,3 @@
 cd src/client/Phm.MobileSp.Cms
-node node_modules/webpack/bin/webpack.js --config ./webpack.config.vendor.js --env.prod
+node node_modules/webpack/bin/webpack.js --config ./src/client/Phm.MobileSp.Cms/webpack.config.vendor.js --env.prod
 node node_modules/webpack/bin/webpack.js --config --env.prod
