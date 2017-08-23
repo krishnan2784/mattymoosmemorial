fe4a9fc7c9be8dc45a7d1ac29f2372ff174d8e47
Surendra Nath <surendra@phm.co.uk>
2017-08-23 15:19:51 +0100

    Modify Repository to follow SOLID and add classes that help us follow SOLID.

---
+++ src/lib/Phm.MobileSp.Cms.Infrastructure/Utilities/CloudContainerDetails.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Phm.MobileSp.Cms.Infrastructure
+{
+    public struct CloudContainerDetails
+    {
+        public string BrandName { get; set; }
+        public CloudBlobContainer Container { get; set; }
+        public string ContainerRoot { get; set; }
+        public int CurrentMarketId { get; set; }
+        public string FileName { get; set; }
+        public long FileSize { get; set; }
+        public string MarketName { get; set; }
+        public string MimeType { get; set; }
+        public string StorageAccountUri { get; set; }
+    }
+
+}
