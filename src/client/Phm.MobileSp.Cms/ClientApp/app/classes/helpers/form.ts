1491643e21075dc8588b3ad1bb35719f3cdf6fd4
Matthew Felton <matthew.felton@phm.co.uk>
2017-09-14 17:12:45 +0100

    merged in changes from dev

---
+++ src/client/Phm.MobileSp.Cms/ClientApp/app/classes/helpers/form.ts
@@ -0,0 +1,23 @@
+﻿export class FormEx {
+    public static getFormValidationErrors(form) {
+        if (!form || !form.controls)
+            return [];
+        var errArray = [];
+        Object.keys(form.controls).forEach(key => {
+            var c = form.get(key);
+            if (c.controls) {
+                var childErrors = this.getFormValidationErrors(c);
+                errArray.concat(childErrors);
+            } else {
+                const controlErrors = c.errors;
+                if (controlErrors != null) {
+                    Object.keys(controlErrors).forEach(keyError => {
+                        errArray.push(c);
+                        //console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ' + controlErrors[keyError]);
+                    });
+                }
+            }
+        });
+        return errArray;
+    }
+}
