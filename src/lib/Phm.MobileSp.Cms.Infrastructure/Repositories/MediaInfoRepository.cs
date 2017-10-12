85530bf7e153c3f3e4acb19763190ed123c272eb
Surendra Nath <surendra@phm.co.uk>
2017-10-12 13:50:06 +0100

    MediaId not Id

--- src/lib/Phm.MobileSp.Cms.Infrastructure/Repositories/MediaInfoRepository.cs
+++ src/lib/Phm.MobileSp.Cms.Infrastructure/Repositories/MediaInfoRepository.cs
@@ -21,7 +21,7 @@ namespace Phm.MobileSp.Cms.Infrastructure.Repositories
 
         public async Task<MediaInfo> CreateMediaInfo(MediaInfo file)
         {
-            file.Id = file.Name;
+            file.MediaId = file.Name;
             var response = await CreateAsync(file);
             return GetResponseModel<MediaInfo>(response);
         }        
