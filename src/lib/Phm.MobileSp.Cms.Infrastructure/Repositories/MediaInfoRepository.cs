64dfbba452865f22014770c75a0153f04ac1f2e2
Surendra Nath <surendra@phm.co.uk>
2017-10-12 13:38:07 +0100

    Data Validation fix.

--- src/lib/Phm.MobileSp.Cms.Infrastructure/Repositories/MediaInfoRepository.cs
+++ src/lib/Phm.MobileSp.Cms.Infrastructure/Repositories/MediaInfoRepository.cs
@@ -21,6 +21,7 @@ namespace Phm.MobileSp.Cms.Infrastructure.Repositories
 
         public async Task<MediaInfo> CreateMediaInfo(MediaInfo file)
         {
+            file.Id = file.Name;
             var response = await CreateAsync(file);
             return GetResponseModel<MediaInfo>(response);
         }        
